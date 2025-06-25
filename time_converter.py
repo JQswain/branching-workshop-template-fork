e953ab02c4ba8efc6b3a1e731727aea5bf1002f9
Gecker0815 <felixgloess@protonmail.com>
2025-06-25 12:54:42 +0200

    Update time_converter.py
    Everything deleted except the function

--- time_converter.py
+++ time_converter.py
@@ -1,11 +1,3 @@
 def seconds_to_minutes(seconds):
     """Converts a time value from seconds to minutes."""
     return seconds / 60
-
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
